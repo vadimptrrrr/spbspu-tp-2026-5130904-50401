5630bde4edc1ebb465d58bb169419a2e877d3dcc
vadim <vadikdydy@mail.ru>
2026-05-23 15:51:16 +0300

    correct

--- petrov.vadim/T2/utils.hpp
+++ petrov.vadim/T2/utils.hpp
@@ -46,16 +46,16 @@ namespace petrov
     std::basic_ios< char > & s_;
     std::streamsize width_;
     std::streamsize precision_;
-    std::basic_ios< char >::fmtflags fmt_;
+    std::basic_ios< char >::fmtflags flags_;
     char fill_;
   };
 
-  std::istream& operator>>(std::istream& in, DelimeterIO dest);
-  std::istream& operator>>(std::istream& in, CharIO dest);
-  std::istream& operator>>(std::istream& in, LabelIO dest);
-  std::istream& operator>>(std::istream& in, StringIO dest);
-  std::istream& operator>>(std::istream& in, DobleLitIO dest);
-  std::istream& operator>>(std::istream& in, DataStruct dest);
+  std::istream& operator>>(std::istream& in, DelimeterIO&& dest);
+  std::istream& operator>>(std::istream& in, CharIO&& dest);
+  std::istream& operator>>(std::istream& in, LabelIO&& dest);
+  std::istream& operator>>(std::istream& in, StringIO&& dest);
+  std::istream& operator>>(std::istream& in, DobleLitIO&& dest);
+  std::istream& operator>>(std::istream& in, DataStruct& dest);
 
   std::ostream& operator<<(std::ostream& os, const DataStruct& data);
 
