8ccb190a58daa3f2574965cfc02e967f2cb2b8f5
vadim <vadikdydy@mail.ru>
2026-05-23 17:50:34 +0300

    compare for datastruct

--- petrov.vadim/T2/utils.cpp
+++ petrov.vadim/T2/utils.cpp
@@ -204,3 +204,15 @@ std::ostream& petrov::operator<<(std::ostream& out, const DataStruct& data)
   return out;
 }
 
+bool petrov::operator<(const DataStruct& lhs, const DataStruct& rhs)
+{
+  if (lhs.key1 != rhs.key1)
+  {
+    return lhs.key1 < rhs.key1;
+  }
+  else if (lhs.key2 != rhs.key2)
+  {
+    return lhs.key2 < rhs.key2;
+  }
+  return lhs.key3 < rhs.key3;
+}
