b0fd433db95148378dbfd77672be502348d19d45
Vadim Petrov <vadikdydy@mail.ru>
2026-06-06 12:15:06 +0300

    add operators for polygon

--- petrov.vadim/T3/geom_shape.cpp
+++ petrov.vadim/T3/geom_shape.cpp
@@ -19,8 +19,8 @@ std::istream& petrov::detail::operator>>(std::istream& is, Point& point)
   {
     return is;
   }
-
   IOguard guard(is);
+
   detail::Point tmp{0, 0};
   using dt_t = DelimeterIO;
   is >> dt_t{'('} >> tmp.x_ >> dt_t{';'} >> tmp.y_ >> dt_t{')'};
@@ -30,3 +30,57 @@ std::istream& petrov::detail::operator>>(std::istream& is, Point& point)
   }
   return is;
 }
+
+std::ostream& petrov::operator<<(std::ostream& os, const Polygon& polygon)
+{
+  std::ostream::sentry sentry(os);
+  if (!sentry)
+  {
+    return os;
+  }
+  IOguard guard(os);
+
+  size_t size = polygon.points_.size();
+  os << size << ' ';
+  using osit_t = std::ostream_iterator< detail::Point >;
+  std::copy_n(polygon.points_.begin(), size - 1, osit_t{os, " "});
+  os << polygon.points_[size-1];
+  return os;
+}
+
+std::istream& petrov::operator>>(std::istream& is, Polygon& polygon)
+{
+  std::istream::sentry sentry(is);
+  if (!sentry)
+  {
+    return is;
+  }
+  IOguard guard(is);
+
+  size_t size = 0;
+  is >> size;
+
+  if (!is || size < 3)
+  {
+    is.setstate(std::ios_base::failbit);
+    polygon.points_.clear();
+    return is;
+  }
+
+  std::vector< detail::Point > tmp;
+  tmp.reserve(size);
+  using isit_t = std::istream_iterator< detail::Point >;
+  std::copy_n(isit_t{is}, size, std::back_inserter(tmp));
+  if (is && tmp.size() == size)
+  {
+    polygon.points_ = std::move(tmp);
+  }
+  else
+  {
+    is.setstate(std::ios_base::failbit);
+    polygon.points_.clear();
+  }
+
+  return is;
+}
+
